2e02e4145b80b77a12325faad19a9960950f982a
Reyhan Huditama <reyhan21098@gmail.com>
2019-04-11 20:20:45 +0700

    Finished login and register

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,9 +1,28 @@
 Vue.component('navbar', {
-    props: ['isLoggedIn'],
+    props: ['isLoggedIn', 'currentPage'],
     data: function () {
         return {
             email: '',
             password: ''
+        }
+    },
+    methods: {
+        logout: function () {
+            swal('Success!', 'Bye!', 'success')
+            this.$emit('logout')
+            this.$emit('get-home-page')
+            localStorage.removeItem('UserId')
+            localStorage.removeItem('token')
+            localStorage.removeItem('email')
+        },
+        getHomePage() {
+            this.$emit('get-home-page')
+        },
+        getLoginPage() {
+            this.$emit('get-login-page')
+        },
+        getRegisterPage() {
+            this.$emit('get-register-page')
         }
     },
     template: `
@@ -16,15 +35,15 @@ Vue.component('navbar', {
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#" v-on:click="getHomePage">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item" v-if="!isLoggedIn">
-                        <a class="nav-link" href="#">Register</a>
+                        <a class="nav-link" href="#" v-on:click="getRegisterPage">Register</a>
                     </li>
                     <li class="nav-item" v-if="!isLoggedIn">
-                        <a class="nav-link" href="#">Login</a>
+                        <a class="nav-link" href="#" v-on:click="getLoginPage">Login</a>
                     </li>
-                    <li class="nav-item" v-if="isLoggedIn">
+                    <li class="nav-item" v-if="isLoggedIn" v-on:click="logout">
                         <a class="nav-link" href="#">Logout</a>
                     </li>
                 </ul>
--- js/main.js
+++ js/main.js
@@ -3,6 +3,29 @@ const serverUrl = `http://localhost:3000`
 let app = new Vue({
     el: '#app',
     data: {
-        isLoggedIn: true
+        isLoggedIn: false,
+        currentPage: 'homepage'
+    },
+    created() {
+        const token = localStorage.getItem('token')
+        if (token) this.verify()
+    },
+    methods: {
+        verify: function () {
+            let token = localStorage.getItem('token')
+            axios
+                .post(`${serverUrl}/verify`, { token }, { headers: { token } })
+                .then(({ data }) => {
+                    this.isLoggedIn = true
+                    console.log(data.message)
+                })
+                .catch((err) => {
+                    const { message } = err.response.data
+                    swal("Error!", message, "error");
+                    localStorage.removeItem('token')
+                    localStorage.removeItem('UserId')
+                    localStorage.removeItem('email')
+                })
+        }
     }
 })
