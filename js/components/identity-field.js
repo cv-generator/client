e4440f541fccf9a1016d0079d6e76030e61e5620
Hindya Pwayari Razzaq <hindyapr@gmail.com>
2019-04-12 15:22:41 +0700

    edit bug identity-field

--- js/components/identity-field.js
+++ js/components/identity-field.js
@@ -12,10 +12,10 @@ Vue.component('identity-field', {
             <img src="https://dummyimage.com/300.png" alt="dummy profile picture">
         </figure>
         <div class="intro">
-            <!-- <h1 class="fullname" v-if="preview">{{ nama }}</h1> -->
+            <p class="fullname" v-if="preview">{{ nama }}</p>
             <input style="" type="text" class="fullname camouflage" placeholder="fullname" onkeypress="this.style.width = ((this.value.length + 9)* 24) + 'px';" v-model="nama" v-if="!preview">
             <div class="divider"></div>
-            <!-- <p class="profesi" v-if="preview">{{ profesi }}</p> -->
+            <p class="profesi" v-if="preview">{{ profesi }}</p>
             <input style="" type="text" class="profesi camouflage text-bold" placeholder="profesi" onkeypress="this.style.width = ((this.value.length + 4)* 12) + 'px';" v-model="profesi" v-if="!preview">
         </div>
     </div>
